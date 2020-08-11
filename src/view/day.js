f71d03a83136f7fc52c58ae9dc0f8a2a86b7ecdd
Alex <alex1031alex@yandex.ru>
2020-08-11 21:11:44 +0300

    3.1.13 В компоненте day добавит атрибуту datetime динамически меняющееся значение.

--- src/view/day.js
+++ src/view/day.js
@@ -3,11 +3,10 @@ export const createDayTemplate = (number, date) => {
     `<li class="trip-days__item  day" data-day="${number}">
       <div class="day__info">
         <span class="day__counter">${number}</span>
-        <time class="day__date" datetime="2019-03-18">${date.toLocaleString(`en-US`, {month: `short`, day: `2-digit`})}</time>
+        <time class="day__date" datetime="${date.getFullYear()}-${date.toLocaleString(`en-US`, {month: `2-digit`})}-${date.toLocaleString(`en-US`, {day: `2-digit`})}">${date.toLocaleString(`en-US`, {month: `short`, day: `2-digit`})}</time>
       </div>
 
       <ul class="trip-events__list">
-
       </ul>
     </li>`
   );
